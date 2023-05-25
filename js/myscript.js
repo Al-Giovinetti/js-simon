a6f42d4b1f23f1896f2c57cbf70fa90efdfc32b9
Al-Giovinetti <giovinettialessio@gmail.com>
2023-05-25 17:26:43 +0200

    Dichiarate variabili per pagina

--- js/myscript.js
+++ js/myscript.js
@@ -1,17 +1,24 @@
-const clock = setInterval(getMyDate,1000)
+let today = new Date();
+today=(today[Symbol.toPrimitive]('number'));
 
-console.log(clock)
+let date1 = new Date('26 May 2023 09:30');
+date1=(date1[Symbol.toPrimitive]('number'));
+
+let gap = date1 - today;
+console.log(gap)
+
+let myDay = document.getElementById("days")
+let myHours = document.getElementById("hours")
+let myMinuts = document.getElementById("minuts")
+let mySeconds = document.getElementById("seconds")
 
 
 
-//FUNCTION//
 
-/**
- * function that returns the current date
- * 
- * @returns current date,hours,minuts ... 
- */
-function getMyDate(){
-    const date = new Date () 
-    return date
-}
+
+
+
+
+
+
+
