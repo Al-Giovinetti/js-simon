fe6a6805a8b8b3c0d01310eb9714cae498a8fb5e
Al-Giovinetti <giovinettialessio@gmail.com>
2023-05-25 17:53:01 +0200

    Quanto manca alla data scelta

--- js/myscript.js
+++ js/myscript.js
@@ -12,6 +12,18 @@ let myHours = document.getElementById("hours")
 let myMinuts = document.getElementById("minuts")
 let mySeconds = document.getElementById("seconds")
 
+myDay = Math.floor(gap / 86400000)
+console.log(myDay)
+myHours = Math.floor((gap - myDay * 86400000) / 3600000)
+console.log(myHours)
+myMinuts = Math.floor((gap - myHours * 3600000) / 60000)
+console.log(myMinuts)
+mySeconds = Math.floor((gap - (myMinuts * 60000 + myHours * 3600000)) / 1000)
+console.log(mySeconds)
+
+
+
+
 
 
 
