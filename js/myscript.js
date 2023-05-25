2cc0ecbb31c14b81ecf2441333806f393afedb54
Al-Giovinetti <giovinettialessio@gmail.com>
2023-05-25 18:30:52 +0200

    Stampa in pagina

--- js/myscript.js
+++ js/myscript.js
@@ -7,19 +7,27 @@ date1=(date1[Symbol.toPrimitive]('number'));
 let gap = date1 - today;
 console.log(gap)
 
-let myDay = document.getElementById("days")
+let myDays = document.getElementById("days")
 let myHours = document.getElementById("hours")
 let myMinuts = document.getElementById("minuts")
 let mySeconds = document.getElementById("seconds")
 
-myDay = Math.floor(gap / 86400000)
-console.log(myDay)
-myHours = Math.floor((gap - myDay * 86400000) / 3600000)
-console.log(myHours)
-myMinuts = Math.floor((gap - myHours * 3600000) / 60000)
-console.log(myMinuts)
-mySeconds = Math.floor((gap - (myMinuts * 60000 + myHours * 3600000)) / 1000)
-console.log(mySeconds)
+let appDay = Math.floor(gap / 86400000)
+console.log(appDay)
+
+let appHours = Math.floor((gap - appDay * 86400000) / 3600000)
+console.log(appHours)
+
+let appMinuts = Math.floor((gap - appHours * 3600000) / 60000)
+console.log(appMinuts)
+
+let appSeconds = Math.floor((gap - (appMinuts * 60000 + appHours * 3600000)) / 1000)
+console.log(appSeconds)
+
+myDays.innerHTML = appDay
+myHours.innerHTML = appHours
+myMinuts.innerHTML = appMinuts
+mySeconds.innerHTML = appSeconds
 
 
 
