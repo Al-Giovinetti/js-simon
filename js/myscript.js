4d5c74601ad61acdc881b853981255cd493ef8c9
Al-Giovinetti <giovinettialessio@gmail.com>
2023-06-05 00:03:08 +0200

    Timer sistemato

--- js/myscript.js
+++ js/myscript.js
@@ -1,59 +1,42 @@
-let today = setInterval(myDate,1000)
-console.log(today)
-let isWeekend= today.getDay()
-today=(today[Symbol.toPrimitive]('number'));
+const clock = setInterval(countdown,1000)
 
-let date0 = new Date('26 May 2023 09:30');
-date1=(date0[Symbol.toPrimitive]('number'));
+function countdown(){
+    let now = new Date ()
+    const dayWeek = now.getDay()
+    now = now.valueOf()
 
-let gap;
+    let appointment = new Date ('05 Jun 2023 09:30:00')
+    appointment = appointment.valueOf()
+        
+    let gap = appointment - now
 
- gap = date0 - today;
-console.log(gap)
+    const second = 1000
+    const minute = second * 60
+    const hour = minute * 60
+    const day = hour * 24 
 
-if((gap == 0)&&(isWeekend<5)){
-    gap = 86400000
-}else if((gap == 0)&&(isWeekend>=5)){
-    gap = 86400000*2
+    if(gap < 0){
+        if(dayWeek == 6){
+            gap =  (appointment + day * 2)  - now 
+        }else if(dayWeek == 5 ){
+            gap = gap =  (appointment + day * 3)  - now 
+        }else{
+            gap = gap =  (appointment + day )  - now 
+        }
+    } 
+
+    console.log(gap)
+
+
+    const daysCountdown = document.getElementById("days")
+    const hoursCountdown = document.getElementById("hours")
+    const minuteCountdown = document.getElementById("minutes")
+    const secondCountdown = document.getElementById("seconds")
+
+    daysCountdown.innerHTML = Math.floor((gap / day) % 24);
+    hoursCountdown.innerHTML = Math.floor((gap / hour ) % 60);
+    minuteCountdown.innerHTML = Math.floor (( gap / minute) % 60);
+    secondCountdown.innerHTML = Math.floor (( gap /second) % 60);
 }
-
-let myDays = document.getElementById("days")
-let myHours = document.getElementById("hours")
-let myMinuts = document.getElementById("minuts")
-let mySeconds = document.getElementById("seconds")
-
-let appDay = Math.floor(gap / 86400000)
-console.log(appDay)
-
-let appHours = Math.floor((gap - appDay * 86400000) / 3600000)
-console.log(appHours)
-
-let appMinuts = Math.floor((gap - appHours * 3600000) / 60000)
-console.log(appMinuts)
-
-let appSeconds = Math.floor((gap - (appMinuts * 60000 + appHours * 3600000)) / 1000)
-console.log(appSeconds)
-
-myDays.innerHTML = appDay
-myHours.innerHTML = appHours
-myMinuts.innerHTML = appMinuts
-mySeconds.innerHTML = appSeconds
-
-const clock = setInterval(myDate,1000)
-
-
-function myDate(){
-    let date = new Date()
-    return date
-}
-
-
-
-
-
-
-
-
-
 
 
